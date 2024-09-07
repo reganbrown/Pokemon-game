9a6235a36955ad4a8575dd589f531ce63f5601a9
reganbrown <regan_brown@icloud.com>
2024-09-06 23:59:03 -0300

    create johoto array and player hand

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,16 @@
-const pokemonStartersEl = document.querySelector(".pokemon-starters");
+const pokemonStartersEl = document.querySelector(".player-starters");
+const pokemonStartersPC = document.querySelector(".computer-starters");
 const pokemonStarterGreenEl = document.querySelector(".pokemon-card-green");
 const pokemonStarterRedEl = document.querySelector(".pokemon-card-red");
 const pokemonStarterBlueEl = document.querySelector(".pokemon-card-blue");
+const pokemonStarterGreenPC = document.querySelector(".pokemon-card-green-PC");
+const pokemonStarterRedPC = document.querySelector(".pokemon-card-red-PC");
+const pokemonStarterBluePC = document.querySelector(".pokemon-card-blue-PC");
+
 const pokemonAPI = "https://pokeapi.co/api/v2/pokemon/";
-const allPokemon = ["1", "4", "7"];
+const kantoPokemon = ["1", "4", "7"];
+const johotoPokemon = ["152", "155", "158"];
+
 const bulbButton = document.querySelector(".bulb");
 const charButton = document.querySelector(".char");
 const squirButton = document.querySelector(".squir");
@@ -11,9 +18,9 @@ const winnerBox = document.createElement("div");
 winnerBox.classList.add("winner-box");
 const winner = "WINNER!";
 winnerBox.innerText = winner;
+
 const fetchPokemonGreen = async (pokeURL) => {
   const response = await axios.get(pokeURL);
-  console.log(response);
   const newPokemonCard = document.createElement("div");
   newPokemonCard.classList.add("pokemon-card");
 
@@ -38,7 +45,6 @@ const fetchPokemonGreen = async (pokeURL) => {
 
 const fetchPokemonBlue = async (pokeURL) => {
   const response = await axios.get(pokeURL);
-  console.log(response);
   const newPokemonCard = document.createElement("div");
   newPokemonCard.classList.add("pokemon-card");
 
@@ -63,7 +69,6 @@ const fetchPokemonBlue = async (pokeURL) => {
 
 const fetchPokemonRed = async (pokeURL) => {
   const response = await axios.get(pokeURL);
-  console.log(response);
   const newPokemonCard = document.createElement("div");
   newPokemonCard.classList.add("pokemon-card");
 
@@ -87,42 +92,56 @@ const fetchPokemonRed = async (pokeURL) => {
 };
 
 function printPokemon() {
-  fetchPokemonGreen(pokemonAPI + allPokemon[0]);
-  fetchPokemonRed(pokemonAPI + allPokemon[1]);
-  fetchPokemonBlue(pokemonAPI + allPokemon[2]);
-  if (allPokemon[0] == "3" && allPokemon[1] == "6" && allPokemon[2] == "9") {
+  fetchPokemonGreen(pokemonAPI + kantoPokemon[0]);
+  fetchPokemonRed(pokemonAPI + kantoPokemon[1]);
+  fetchPokemonBlue(pokemonAPI + kantoPokemon[2]);
+  if (
+    kantoPokemon[0] == "3" &&
+    kantoPokemon[1] == "6" &&
+    kantoPokemon[2] == "9"
+  ) {
     pokemonStartersEl.appendChild(winnerBox);
   }
 }
 
-printPokemon();
-
 function evolveBulb() {
-  if (allPokemon[0] !== "3") {
+  if (kantoPokemon[0] !== "3") {
     pokemonStarterGreenEl.innerHTML = "";
     pokemonStarterBlueEl.innerHTML = "";
     pokemonStarterRedEl.innerHTML = "";
-    allPokemon[0] = (Number(allPokemon[0]) + 1).toString();
+    pokemonStarterGreenPC.innerHTML = "";
+    pokemonStarterBluePC.innerHTML = "";
+    pokemonStarterRedPC.innerHTML = "";
+    kantoPokemon[0] = (Number(kantoPokemon[0]) + 1).toString();
+    printPokemonPC();
     printPokemon();
   }
 }
 
 function evolveChar() {
-  if (allPokemon[1] !== "6") {
+  if (kantoPokemon[1] !== "6") {
     pokemonStarterGreenEl.innerHTML = "";
     pokemonStarterBlueEl.innerHTML = "";
     pokemonStarterRedEl.innerHTML = "";
-    allPokemon[1] = (Number(allPokemon[1]) + 1).toString();
+    pokemonStarterGreenPC.innerHTML = "";
+    pokemonStarterBluePC.innerHTML = "";
+    pokemonStarterRedPC.innerHTML = "";
+    kantoPokemon[1] = (Number(kantoPokemon[1]) + 1).toString();
+    printPokemonPC();
     printPokemon();
   }
 }
 
 function evolveSquir() {
-  if (allPokemon[2] !== "9") {
+  if (kantoPokemon[2] !== "9") {
     pokemonStarterGreenEl.innerHTML = "";
     pokemonStarterBlueEl.innerHTML = "";
     pokemonStarterRedEl.innerHTML = "";
-    allPokemon[2] = (Number(allPokemon[2]) + 1).toString();
+    pokemonStarterGreenPC.innerHTML = "";
+    pokemonStarterBluePC.innerHTML = "";
+    pokemonStarterRedPC.innerHTML = "";
+    kantoPokemon[2] = (Number(kantoPokemon[2]) + 1).toString();
+    printPokemonPC();
     printPokemon();
   }
 }
@@ -130,3 +149,86 @@ function evolveSquir() {
 bulbButton.addEventListener("click", evolveBulb);
 charButton.addEventListener("click", evolveChar);
 squirButton.addEventListener("click", evolveSquir);
+
+/// PC
+
+const fetchPokemonGreenPC = async (pokeURL) => {
+  const response = await axios.get(pokeURL);
+  const newPokemonCard = document.createElement("div");
+  newPokemonCard.classList.add("pokemon-card");
+
+  const pokemonPic = response.data.sprites.front_default;
+  const newPokemonPic = document.createElement("img");
+  newPokemonPic.classList.add("pokemon-card__img");
+  newPokemonPic.setAttribute("src", pokemonPic);
+
+  newPokemonCard.appendChild(newPokemonPic);
+
+  const newPokemonName = document.createElement("p");
+  newPokemonName.classList.add("pokemon-card__name");
+  newPokemonName.innerText = response.data.name;
+  newPokemonCard.appendChild(newPokemonName);
+
+  const newPokemonType = document.createElement("p");
+  newPokemonType.classList.add("pokemon-card__type");
+  newPokemonType.innerText = response.data.types[0].type.name;
+  newPokemonCard.appendChild(newPokemonType);
+  pokemonStarterGreenPC.appendChild(newPokemonCard);
+};
+
+const fetchPokemonBluePC = async (pokeURL) => {
+  const response = await axios.get(pokeURL);
+  const newPokemonCard = document.createElement("div");
+  newPokemonCard.classList.add("pokemon-card");
+
+  const pokemonPic = response.data.sprites.front_default;
+  const newPokemonPic = document.createElement("img");
+  newPokemonPic.classList.add("pokemon-card__img");
+  newPokemonPic.setAttribute("src", pokemonPic);
+
+  newPokemonCard.appendChild(newPokemonPic);
+
+  const newPokemonName = document.createElement("p");
+  newPokemonName.classList.add("pokemon-card__name");
+  newPokemonName.innerText = response.data.name;
+  newPokemonCard.appendChild(newPokemonName);
+
+  const newPokemonType = document.createElement("p");
+  newPokemonType.classList.add("pokemon-card__type");
+  newPokemonType.innerText = response.data.types[0].type.name;
+  newPokemonCard.appendChild(newPokemonType);
+  pokemonStarterBluePC.appendChild(newPokemonCard);
+};
+
+const fetchPokemonRedPC = async (pokeURL) => {
+  const response = await axios.get(pokeURL);
+  const newPokemonCard = document.createElement("div");
+  newPokemonCard.classList.add("pokemon-card");
+
+  const pokemonPic = response.data.sprites.front_default;
+  const newPokemonPic = document.createElement("img");
+  newPokemonPic.classList.add("pokemon-card__img");
+  newPokemonPic.setAttribute("src", pokemonPic);
+
+  newPokemonCard.appendChild(newPokemonPic);
+
+  const newPokemonName = document.createElement("p");
+  newPokemonName.classList.add("pokemon-card__name");
+  newPokemonName.innerText = response.data.name;
+  newPokemonCard.appendChild(newPokemonName);
+
+  const newPokemonType = document.createElement("p");
+  newPokemonType.classList.add("pokemon-card__type");
+  newPokemonType.innerText = response.data.types[0].type.name;
+  newPokemonCard.appendChild(newPokemonType);
+  pokemonStarterRedPC.appendChild(newPokemonCard);
+};
+
+function printPokemonPC() {
+  fetchPokemonGreenPC(pokemonAPI + johotoPokemon[0]);
+  fetchPokemonRedPC(pokemonAPI + johotoPokemon[1]);
+  fetchPokemonBluePC(pokemonAPI + johotoPokemon[2]);
+}
+
+printPokemonPC();
+printPokemon();
